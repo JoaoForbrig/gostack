2eb8d33930fae1a10d0c0f3f02c77f1066e0c82b
João Forbrig <62452520+jonnyrei789@users.noreply.github.com>
2020-08-31 23:00:16 -0300

    exec

--- projetoum/src/routes/appointments.routes.ts
+++ projetoum/src/routes/appointments.routes.ts
@@ -1,11 +1,17 @@
 import {Router} from 'express';
 import {startOfHour, parseISO, isEqual} from 'date-fns';
 
-import Appointment from '../models/Appointment';
+
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) =>{
+  const appointments = appointmentsRepository.all()
+
+  return response.json(appointments)
+})
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } =  request.body;
