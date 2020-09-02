9e159e4907b30878da5917c234f7f95bde53ab78
João Forbrig <62452520+jonnyrei789@users.noreply.github.com>
2020-09-02 15:58:46 -0300

    react proj

---
+++ primeiro-projeto-react/src/App.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/jsx-indent */
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes/index';
+
+const App: React.FC = () => (
+    <BrowserRouter>
+        <Routes />
+    </BrowserRouter>
+);
+export default App;
