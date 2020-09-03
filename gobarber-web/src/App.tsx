cc403182f1d91f67d7c66032e63d3a5f497d2f40
João Forbrig <62452520+jonnyrei789@users.noreply.github.com>
2020-09-03 20:30:44 -0300

    commit chenges

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>Hello World</h1>;
+import GlobalStyle from './styles/global';
+import SignIn from './pages/SignIn/index';
 
+const App: React.FC = () => (
+    <>
+        <SignIn />
+        <GlobalStyle />
+    </>
+);
 export default App;
---
+++ gobarber-web/src/components/Input/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background: #232129;
+    border-radius: 10px;
+    border: 2px solid #232129;
+    padding: 16px;
+    width: 100%;
+    color: #666360;
+
+    & + div {
+        margin-top: 8px;
+    }
+    display: flex;
+    align-items: center;
+    input {
+        background: transparent;
+        flex: 1;
+        border: 0;
+        color: #f4ede8;
+
+        &::placeholder {
+            color: #666360;
+        }
+    }
+
+    svg {
+        margin-right: 16px;
+    }
+`;
