ba8be837d587c72e2d025f71afe7b8199a4e5595
João Forbrig <62452520+jonnyrei789@users.noreply.github.com>
2020-09-03 10:52:37 -0300

    projeto-Github-Repositorio

--- primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,7 +1,88 @@
-import React from 'react';
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable camelcase */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable react/jsx-indent */
+import React, { useState, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
 
 const Dashboard: React.FC = () => {
-    return <h1>Dashboard</h1>;
+    const [newRepo, setNewRepo] = useState('');
+    const [inputError, setInputError] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    async function handleAddRepository(
+        event: FormEvent<HTMLFormElement>,
+    ): Promise<void> {
+        // adição de um novo Repositorio
+        // Consumir API do Github
+        // Salvar novo repositorio no estado
+        event.preventDefault();
+
+        if (!newRepo) {
+            setInputError('Digite o autor/nome do repositório');
+            return;
+        }
+
+        try {
+            const response = await api.get<Repository>(`repos/${newRepo}`);
+
+            const repository = response.data;
+
+            setRepositories([...repositories, repository]);
+            setNewRepo('');
+            setInputError('');
+        } catch (err) {
+            setInputError('Erro na busca por esse repositório');
+        }
+    }
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Title>Explore Repositórios no Github</Title>
+
+            <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+                <input
+                    value={newRepo}
+                    onChange={e => setNewRepo(e.target.value)}
+                    placeholder="Digite o nome do repositório"
+                />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            {inputError && <Error> {inputError} </Error>}
+
+            <Repositories>
+                {repositories.map(repository => (
+                    <a key={repository.full_name} href="teste">
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                        />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                        <FiChevronRight size={20} />
+                    </a>
+                ))}
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
