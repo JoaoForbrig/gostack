b3bf2fdeab852874eab3d74ce37a016fedd20139
João Forbrig <62452520+jonnyrei789@users.noreply.github.com>
2020-09-06 12:58:13 -0300

    sundey service

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -4,10 +4,15 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/Signin';
 // import SignUp from './pages/SignUp';
 
+import { AuthProvider } from './context/AuthContext';
+
 const App: React.FC = () => (
     <>
-        <SignIn />
+        <AuthProvider>
+            <SignIn />
+        </AuthProvider>
         <GlobalStyle />
     </>
 );
+
 export default App;
--- gobarber-web/src/pages/Signin/index.tsx
+++ gobarber-web/src/pages/Signin/index.tsx
@@ -1,9 +1,12 @@
-import React, { useRef, useCallback } from 'react';
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-
+import { AuthContext } from '../../context/AuthContext';
 import logoImg from '../../assets/logo.svg';
 import getValidationErros from '../../utils/getValidationErrors';
 
@@ -12,31 +15,47 @@ import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+interface SignInFormData {
+    email: string;
+    password: string;
+}
+
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
 
+    const { user, signIn } = useContext(AuthContext);
+
+    console.log(user);
+
     // eslint-disable-next-line @typescript-eslint/ban-types
-    const handleSubmit = useCallback(async (data: object) => {
-        try {
-            formRef.current?.setErrors({});
+    const handleSubmit = useCallback(
+        async (data: SignInFormData) => {
+            try {
+                formRef.current?.setErrors({});
 
-            const schema = Yup.object().shape({
-                email: Yup.string()
-                    .required('E-mail obrigatório')
-                    .email('Digite um email válido'),
-                password: Yup.string().required('Senha obrigatória'),
-            });
-            await schema.validate(data, {
-                abortEarly: false,
-            });
-        } catch (err) {
-            // eslint-disable-next-line no-console
-            console.log(err);
+                const schema = Yup.object().shape({
+                    email: Yup.string()
+                        .required('E-mail obrigatório')
+                        .email('Digite um email válido'),
+                    password: Yup.string().required('Senha obrigatória'),
+                });
+                await schema.validate(data, {
+                    abortEarly: false,
+                });
+                signIn({
+                    email: data.email,
+                    password: data.password,
+                });
+            } catch (err) {
+                // eslint-disable-next-line no-console
+                console.log(err);
 
-            const errors = getValidationErros(err);
-            formRef.current?.setErrors(errors);
-        }
-    }, []);
+                const errors = getValidationErros(err);
+                formRef.current?.setErrors(errors);
+            }
+        },
+        [signIn],
+    );
     return (
         <Container>
             <Content>
