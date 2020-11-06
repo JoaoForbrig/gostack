b44deb46df1452bf6a72b8cb53b4d61266d9d20a
João Forbrig <62452520+jonnyrei789@users.noreply.github.com>
2020-11-06 15:08:52 -0300

    reparando compareDate

--- projetoum/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ projetoum/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,12 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable class-methods-use-this */
 /* eslint-disable camelcase */
-import 'reflect-metadata';
 import { injectable, inject } from 'tsyringe';
-import { getDaysInMonth, getDate } from 'date-fns';
+import { getDaysInMonth, getDate, isAfter } from 'date-fns';
+
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
-interface Request {
+interface IRequest {
   provider_id: string;
   month: number;
   year: number;
@@ -26,10 +24,10 @@ class ListProviderMonthAvailabilityService {
 
   public async execute({
     provider_id,
-    month,
     year,
-  }: Request): Promise<IResponse> {
-    const appoitments = await this.appointmentsRepository.findAllInMonthFromProvider(
+    month,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
       {
         provider_id,
         year,
@@ -45,16 +43,21 @@ class ListProviderMonthAvailabilityService {
     );
 
     const availability = eachDayArray.map(day => {
-      const appointmentsInDay = appoitments.filter(appoitment => {
-        return getDate(appoitment.date) === day;
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
+
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
       });
 
       return {
         day,
-        available: appointmentsInDay.length < 10,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
       };
     });
+
     return availability;
   }
 }
+
 export default ListProviderMonthAvailabilityService;
