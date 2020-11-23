a2d7eaf91f7832537de96f30a2b5e6507fecd395
João Forbrig <62452520+jonnyrei789@users.noreply.github.com>
2020-11-23 13:59:17 -0300

    Remove the now ignored directory node_modules

--- projetoum/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ projetoum/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -1,17 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import AppError from '@shared/errors/AppError';
+import 'reflect-metadata';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import ListProviderMonthAvailabilityService from './ListProviderMonthAvailabilityService';
-import AppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 
-let fakeAppointmentsRepository: AppointmentsRepository;
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
 let listProviderMonthAvailability: ListProviderMonthAvailabilityService;
+
 describe('ListProviderMonthAvailability', () => {
   beforeEach(() => {
-    fakeAppointmentsRepository = new AppointmentsRepository();
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
     listProviderMonthAvailability = new ListProviderMonthAvailabilityService(
       fakeAppointmentsRepository,
     );
   });
+
   it('should be able to list the month availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
@@ -28,28 +30,24 @@ describe('ListProviderMonthAvailability', () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
       user_id: 'user',
-
       date: new Date(2020, 4, 20, 10, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
       user_id: 'user',
-
       date: new Date(2020, 4, 20, 11, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
       user_id: 'user',
-
       date: new Date(2020, 4, 20, 12, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
       user_id: 'user',
-
       date: new Date(2020, 4, 20, 13, 0, 0),
     });
 
