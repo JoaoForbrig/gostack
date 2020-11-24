69609ac9fd5fdcd2b591196dec14ec1effbb164e
João Forbrig <62452520+jonnyrei789@users.noreply.github.com>
2020-11-24 12:03:57 -0300

    ok

--- projetoum/src/modules/users/repositories/IUsersRepository.ts
+++ projetoum/src/modules/users/repositories/IUsersRepository.ts
@@ -3,7 +3,6 @@ import User from '../infra/typeorm/entities/User';
 import ICreateUserDTO from '../dtos/ICreateUserDTO';
 import IFindAllProvidersDTO from '../dtos/IFindAllProvidersDTO';
 
-
 export default interface IUsersRepository {
   findAllProviders(data: IFindAllProvidersDTO): Promise<User[]>;
   findById(id: string): Promise<User | undefined>;
